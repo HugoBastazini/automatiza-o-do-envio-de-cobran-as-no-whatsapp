4aca545721b720366c66a9466b8dac4e9557005f
Hugo Bastazini <hugo.bastazini0412@gmail.com>
2024-10-30 08:39:03 -0300

    style: segundo frame

--- src/gui/widgets.py
+++ src/gui/widgets.py
@@ -6,6 +6,8 @@ class Widgets():
         self.Frames()
 
     def Frames(self):
-        self.frame1 = Frame(self.root)
-        self.frame1.place(relx=0.01, rely=0.03, relwidth=0.98, relheight=0.5)
+        self.frame1 = Frame(self.root, bd=2, highlightbackground="black", highlightthickness=2)
+        self.frame1.place(relx=0.01, rely=0.03, relwidth=0.98, relheight=0.64)
+        self.frame2 = Frame(self.root, bd=2, highlightbackground="black", highlightthickness=2)
+        self.frame2.place(relx=0.01, rely=0.70, relwidth=0.98, relheight=0.28)
 
