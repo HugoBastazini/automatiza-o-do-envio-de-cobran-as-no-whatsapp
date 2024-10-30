0dd3ac9e6ad607e5137cdb6fcfa247c88302a792
Hugo Bastazini <hugo.bastazini0412@gmail.com>
2024-10-30 14:47:54 -0300

    style: criações de widgets diversos

--- src/gui/widgets.py
+++ src/gui/widgets.py
@@ -20,7 +20,7 @@ class Widgets():
         self.botaoBuscar = Button(self.frame1, text="BUSCAR")
         self.botaoBuscar.place(relx=0.89, rely=0.01, relwidth=0.1, relheight=0.08)
         self.botaoUpload = Button(self.frame1, text="IMAGEM", command=self.upload_imagem)
-        self.botaoUpload.place(relx=0.78, rely=0.01, relwidth=0.1, relheight=0.08)
+        self.botaoUpload.place(relx=0.53, rely=0.01, relwidth=0.1, relheight=0.08)
     def LabelsEntry(self):
         self.lbMensagem = Label(self.frame1, text="Mensagem", background="#DCDCDC")
         self.lbMensagem.place(relx=0.01, rely=0.13)
@@ -29,7 +29,19 @@ class Widgets():
         self.lbLink = Label(self.frame1, text="Link", background="#DCDCDC")
         self.lbLink.place(relx=0.35, rely=0.13)
         self.etLink = Entry(self.frame1)
-        self.etLink.place(relx=0.35, rely=0.23)
+        self.etLink.place(relx=0.35, rely=0.23, relwidth=0.2)
+        self.lbNome = Label(self.frame1, text="Nome", background="#DCDCDC")
+        self.lbNome.place(relx=0.35, rely=0.33)
+        self.etNome = Entry(self.frame1)
+        self.etNome.place(relx=0.35, rely=0.43)
+        self.lbData = Label(self.frame1, text="Data", background="#DCDCDC")
+        self.lbData.place(relx=0.35, rely=0.53)
+        self.etData = Entry(self.frame1)
+        self.etData.place(relx=0.35, rely=0.63, relwidth=0.08)
+        self.lbTitulo = Label(self.frame1, text="CONSTRUVIDA", background="#DCDCDC", font=("Arial", 20))
+        self.lbTitulo.place(relx=0.23, rely=0.003)
+        self.etBusca = Entry(self.frame1)
+        self.etBusca.place(relx=0.67, rely=0.03, relwidth=0.2)
     def upload_imagem(self):
         caminho_imagem = filedialog.askopenfilename(
             title="Selecione uma imagem para upload",
