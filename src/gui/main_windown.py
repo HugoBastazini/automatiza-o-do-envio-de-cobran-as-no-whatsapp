8785d19ecb9ca61e99b720d05cf9fc7a123c2c05
Hugo Bastazini <hugo.bastazini0412@gmail.com>
2024-10-30 11:02:57 -0300

    style: merge das alterações de estilo gerais

--- src/gui/main_windown.py
+++ src/gui/main_windown.py
@@ -11,10 +11,9 @@ class Application():
         root.mainloop()
     def Tela(self):
         self.root.title("Automação de Mensagens")
-        self.root.configure(background="#191970")
+        self.root.configure(background="#4682B4")
         self.root.geometry("800x600")
         self.root.resizable(True, True)
-        self.root.maxsize(width=1024, height=768)
         self.root.minsize(width=640, height=480)
 
 Application()
