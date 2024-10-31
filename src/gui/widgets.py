e437b30acdc3551fb8d828aa07826e00f50aa75d
Hugo Bastazini <hugo.bastazini0412@gmail.com>
2024-10-31 09:51:28 -0300

    fix: removendo parte repetida do codigo

--- src/gui/widgets.py
+++ src/gui/widgets.py
@@ -4,18 +4,6 @@ from PIL import Image, ImageTk
 from tkinter import ttk
 from funcs import Funcs
 
-class Funcs():
-    def Limpar(self):
-        self.txtMensagem.delete("1.0", END)
-        self.etLink.delete(0, END)
-        self.etNome.delete(0, END)
-        self.etData.delete(0, END)
-        self.etBusca.delete(0, END)
-    def upload_imagem(self):
-        caminho_imagem = filedialog.askopenfilename(
-            title="Selecione uma imagem para upload",
-            filetypes=[("Arquivos de imagem", "*.jpg *.jpeg *.png *.bmp *.gif")]
-        )
 
 class Widgets(Funcs):
     def __init__(self, root):
